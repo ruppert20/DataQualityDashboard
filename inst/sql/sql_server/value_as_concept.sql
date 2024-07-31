fa0fffe7f6176e5c75511e7d8e6e61b9135e4da5
Matthew Ruppert <ruppert20@gmail.com>
2024-07-31 17:33:54 -0400

    sql bug fixes

--- inst/sql/sql_server/value_as_concept.sql
+++ inst/sql/sql_server/value_as_concept.sql
@@ -1,5 +1,6 @@
 /*********
 VALUE_AS_NUMBER_CHECK
+PLAUSIBLE_VALUE_HIGH - find any MEASUREMENT records that have VALUE_AS_NUMBER with non-null value > plausible high value
 
 Parameters used in this template:
 cdmDatabaseSchema = @cdmDatabaseSchema
@@ -11,17 +12,16 @@ unitConceptId = @unitConceptId
 cohortDefinitionId = @cohortDefinitionId
 cohortDatabaseSchema = @cohortDatabaseSchema
 cohortTableName = @cohortTableName
-XXXSAVE_FULL_RESULTXXX
-XXXQUERYNAME___VALUE_AS_NUMBER_CHECKXXX
 }
+XXXSAVE_FULL_RESULTXXX
+XXXQUERYNAME___VALUE_AS_CONCEPT_CHECKXXX
 **********/
 
 SELECT 
 	m.person_id,
 	m.visit_occurrence_id,
 	m.@cdmFieldName,
-	m.value_as_number,
-	m.unit_concept_id
+	m.value_as_concept_id
 
 FROM @cdmDatabaseSchema.@cdmTableName m
 	{@cohort}?{
@@ -31,6 +31,3 @@ FROM @cdmDatabaseSchema.@cdmTableName m
 	}
 WHERE
 	m.@cdmFieldName IN (@conceptId)
-	{@unitConceptId NOT IN ('NA', -1, False, '')}?{
-		AND m.unit_concept_id = @unitConceptId
-		};
